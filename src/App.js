b4374ad8028baadab37c2794d18ad57bded2c2c6
shahjalalcse <shah-jalal1>
2021-07-05 14:44:32 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.scss';
+
+import AOS from 'aos';
+import { focusHandling } from 'cruip-js-toolkit';
+
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ResetPassword from './pages/ResetPassword';
+
+export const UserContext = createContext();
+
+function App() {
+
+  const [loggedInUser, setLoggedInUser] = useState({});
+
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/reset-password">
+          <ResetPassword />
+        </Route>
+      </Switch>
+    </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/pages/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmYPDnJUGDJWbKjZ09fKsUwKH3h2wlAkc",
+    authDomain: "evatix-b5bdd.firebaseapp.com",
+    projectId: "evatix-b5bdd",
+    storageBucket: "evatix-b5bdd.appspot.com",
+    messagingSenderId: "1042844289179",
+    appId: "1:1042844289179:web:9bc8c4892db83c7dc45e3e"
+  };
+
+  export default firebaseConfig;
